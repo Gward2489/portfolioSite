b046848df7013513e5dc752cc35775d652022d49
Gward2489 <gww8804@gmail.com>
2018-02-06 23:44:05 -0600

    paginating projects

--- app/controllers/projectsCtrl.js
+++ app/controllers/projectsCtrl.js
@@ -2,6 +2,28 @@ angular.module("gwDev")
 .controller("projectsCtrl", function ($window, $scope, $location, dataFactory) {
 
     $scope.projects = dataFactory.getProjects()
+    $scope.projCount = 0
+    for (p in $scope.projects) 
+    {
+        $scope.projCount ++
+    }
+    $scope.projDisplay = "1"
+
+    $scope.rightClick = function (proj)
+    {
+        if (parseInt(proj) < $scope.projCount )
+        {
+            $scope.projDisplay = JSON.stringify(parseInt(proj) + 1)
+        }
+    }
+
+    $scope.leftClick = function (proj) 
+    {
+        if (parseInt(proj) > 1) 
+        {
+            $scope.projDisplay = JSON.stringify(parseInt(proj) - 1)
+        }    
+    }
 
     $scope.linkTo = function (url) {
         $window.open(url)
