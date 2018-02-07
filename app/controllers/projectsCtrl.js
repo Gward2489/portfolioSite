77e968abe67480b8525f98a02bd8b33e955b8531
Gward2489 <gww8804@gmail.com>
2018-02-07 00:26:55 -0600

    working pagination for projects

--- app/controllers/projectsCtrl.js
+++ app/controllers/projectsCtrl.js
@@ -8,12 +8,14 @@ angular.module("gwDev")
         $scope.projCount ++
     }
     $scope.projDisplay = "1"
+    $scope.grabDisplay= "1"
 
     $scope.rightClick = function (proj)
     {
         if (parseInt(proj) < $scope.projCount )
         {
             $scope.projDisplay = JSON.stringify(parseInt(proj) + 1)
+            $scope.grabDisplay= "1"
         }
     }
 
@@ -22,7 +24,25 @@ angular.module("gwDev")
         if (parseInt(proj) > 1) 
         {
             $scope.projDisplay = JSON.stringify(parseInt(proj) - 1)
+            $scope.grabDisplay= "1"
+            
         }    
+    }
+
+    $scope.changeGrab = function (grab)
+    {
+        if ($scope.grabDisplay != grab )
+        {
+            $scope.grabDisplay = grab
+        }
+    }
+
+    $scope.numbClick = function (numb)
+    {
+        if ($scope.projDisplay != numb)
+        {
+            $scope.projDisplay = numb
+        }
     }
 
     $scope.linkTo = function (url) {
